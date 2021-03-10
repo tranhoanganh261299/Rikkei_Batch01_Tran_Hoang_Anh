a7fbaae134ae31a4406851553069084258bd9a58
TranHoangAnh <hoanganhtran669@gmail.com>
2021-03-11 02:11:45 +0700

    11/3

--- Module_5/springboot_tranhoanganh/src/main/java/rtep_k1/comcom/springboot_tranhoanganh/Controller/NhanVienController.java
+++ Module_5/springboot_tranhoanganh/src/main/java/rtep_k1/comcom/springboot_tranhoanganh/Controller/NhanVienController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import rtep_k1.comcom.springboot_tranhoanganh.Model.NhanVien;
 import rtep_k1.comcom.springboot_tranhoanganh.Services.NhanVienService;
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+
 public class NhanVienController {
     @Autowired
     private NhanVienService nhanVienServiceIplm;
@@ -46,7 +47,24 @@ public class NhanVienController {
     @GetMapping("/nhanvien/create")
     public String createNhanVien(Model model){
         model.addAttribute("employment",new NhanVien());
-        return "create_nhanvien";
+        return "/nhanvien/create";
+    }
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveNhanVien(@ModelAttribute("nhanVien") NhanVien nhanVien) {
+        nhanVienServiceIplm.save(nhanVien);
+        return "redirect:/nhanvien";
+    }
+    @RequestMapping("/edit/{MaNhanVien}")
+    public ModelAndView showEditProductPage(@PathVariable(name = "MaNhanVien") int MaNhanVien) {
+        ModelAndView mav = new ModelAndView("/nhanvien/edit_nhanvien");
+        NhanVien nhanVien = nhanVienServiceIplm.get(MaNhanVien);
+        mav.addObject("nhanVien",  nhanVien);
+        return mav;
     }
 
+    @RequestMapping("/delete/{MaNhanVien}")
+    public String deleteProduct(@PathVariable(name = "MaNhanVien") int MaNhanVien) {
+        nhanVienServiceIplm.delete(MaNhanVien);
+        return "redirect:/nhanvien";
+    }
 }
